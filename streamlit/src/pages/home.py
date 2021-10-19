a551db85c1b0a92a9e4ce848295881153a7207c5
Marie-anneMawhin <mmawhin@gmail.com>
2021-10-19 19:58:48 +0100

    updated

--- streamlit/src/pages/home.py
+++ streamlit/src/pages/home.py
@@ -9,8 +9,14 @@ def write():
     The identification and classification of leukocytes, platelets and erythrocytes is crucial in the diagnosis of several hematological diseases, such as infectious diseases or regenerative anaemia.
     A broader approach will aim at classifying other abnormal cells related to acute leukemia.""")
 
-    # st.image("src/data/images/wbc.gif") 
-    st.write("![Your Awsome GIF](https://www.verywellhealth.com/thmb/nP6pLR72nAK3vVEhM1Wc35S32Ek=/1500x1000/filters:no_upscale():max_bytes(150000):strip_icc()/understanding-white-blood-cells-and-counts-2249217_final-0587074073ce4709b41e42a08db0f596.gif)")
+    st.image("https://www.verywellhealth.com/thmb/nP6pLR72nAK3vVEhM1Wc35S32Ek=/1500x1000/filters:no_upscale():max_bytes(150000):strip_icc()/understanding-white-blood-cells-and-counts-2249217_final-0587074073ce4709b41e42a08db0f596.gif",
+    width=450)
+
+
+    st.markdown("""
+    to fill
+    """)
+    
     st.markdown("""## Contributors
     
     Mathieu Sarrat
