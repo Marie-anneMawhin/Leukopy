83c916208eeac94880c410b7a82c15adb069ce49
Marie-anneMawhin <mmawhin@gmail.com>
2021-11-17 21:00:15 +0000

    added plot of rand cells by ori

--- streamlit/src/pages/EDA.py
+++ streamlit/src/pages/EDA.py
@@ -16,14 +16,43 @@ def write():
     with st.expander('show dataframe'):
         st.dataframe(df.head())
 
-    with st.expander('Cell distribution'):
-        st.write('Here is the count of cells per classes')
+    with st.container():
+        st.markdown(
+        '''
+        # Classe distribution
+
+        The following interactive plot shows the count of cells per classes.
+        ''')
 
         fig_distrib = px.histogram(df, x='label',
         template='none',
-        color='label'
+        color='origin',
+        color_discrete_sequence=['#4C78A8',  '#E45756', '#72B7B2'],
         )
         st.plotly_chart(fig_distrib)
+
+        st.markdown(
+        '''
+        We can see on this plot that the segmented neutrophils (SNE) are 
+        overrepresented in the Munich dataset. The lymphocyte population in majority
+        in the Raabin and the Munich dataset. The classes are more balanced in the 
+        Barcelona dataset. Platelets are only present in the later dataset.
+
+
+        # Luminosity
+        ''')
+        
+        st.image('src/data/images/plot_ramdom_cells.jpg',
+        caption='Plot showing random cells for each label and dataset of origin.')
+
+        st.markdown(
+        '''
+        We can see on this image that each dataset present different coloration. Moreover, certain
+        types of cells look fairly similar: for example, monocytes 
+        and immature granulocytes or segmented and banded neutrophils. Some element could
+        potentially interfer with the classification such as the number of 
+        red blood cells in the background or the image color. 
+        ''')
 
     sizes = df.groupby(['height', 'width']).agg(count=('img_path', 'count')).reset_index()
     st.dataframe(sizes)
@@ -35,3 +64,7 @@ def write():
                     #  height=500, width=600).options(line_alpha=0.7,
                     #                                 line_width=2,
                     #                                 fill_alpha=0.04)
+
+import plotly.express as px
+
+print(px.colors.qualitative.T10)
