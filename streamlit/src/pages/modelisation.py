90876c88151a7db5a1b028e7dd86afdf6ad551ef
MSarrat <84069647+MSarrat@users.noreply.github.com>
2021-12-09 13:06:56 +0100

    VGG19 Modelisation

--- streamlit/src/pages/modelisation.py
+++ streamlit/src/pages/modelisation.py
@@ -17,57 +17,87 @@ def write():
 
     if model_choice == "VGG19":
         
-        cont = st.container()
+        # Model structure and methodology
+        st.subheader("""Model structure""")
+        cont_1 = st.container()
+        cont_1.markdown("""We replace the original VGG19 top layers with our own custom classification block :""")
+        cont_1.image('./data/images/vgg19/vgg19_structure.png')
         
-
-        cont.subheader("""Model structure :""")
-        cont.markdown("""We replace the original VGG19 top layers with our own custom classification block.""")
-        cont.image('./data/images/vgg19/vgg19_structure.png', 
-                   caption = "We use the five convolutional blocks of VGG19 ... \n and connect them to the following layers :")
-
-        cont.image('./data/images/vgg19/vgg19_classblock.png', width = 150)
-        
-        cont.markdown("""To train this model, we adopt a **two-stage approach** :""")
-        cont.markdown("""
-                      + **transfer learning from ImageNet** for VGG19 : 
-                      we only train the classification block layers,
-                      + **fine-tuning of the 'block5'** : we train block5 layers (4 Conv2D layers) 
-                      and adjust the weights of the classification block </br>""", unsafe_allow_html = True)
+        cont_1 = st.container()
+        cont_1.markdown("""To train this model, we adopt a **two-stage approach** :""")
+        cont_1.markdown("""
+                      + **transfer learning from ImageNet** for VGG19 : we only train the classification block layers,
+                      + **fine-tuning of the 'block5'**.
+        """)
+                 
+        cont_1.markdown("""
+        In order to reduce a possible overfitting, we implement the following
+        - **Data augmentation** (see below),
+        - **Dropout layers** (after each Dense layer in the last block),
+        - **Class weights** : during training, we penalize more when errors are committed on low population classes (e.g. PMY or BA),
+        - **Early Stopping**.
+        """)
             
-        cont.markdown("""In order to reduce a possible overfitting, we implement the following :""")
-        cont.markdown("""+ **Data augmentation** (see below),""")
-        cont.markdown("""+ **Dropout layers** (after each Dense layer in the last block),""")
-        cont.markdown("""+ **Class weights** : during training, we penalize more when errors are committed on low population classes (e.g. PMY or BA)
-                        """)
-                        
+        cont_1.markdown('''We use **adaptive learning rate** to accelerate the training, convergence 
+                        and to improve generalization.''')
+              
         
-                
-        cont.subheader("""Preprocessing :""")
+        # Preprocessing
+        cont_2 = st.container()        
+        cont_2.subheader("""Preprocessing""")
   
+        cont_2.markdown("""The data flow through this pipeline during training. 
+                      The same steps (except data augmentation) are applied to the test data.""")
+                      
         """### display gif from local file in markdown"""
         file_ = open("./data/images/vgg19/preprocessing_1750_50.gif", "rb")
         contents = file_.read()
         data_url = base64.b64encode(contents).decode("utf-8")
         file_.close()
 
-        cont.markdown(f'<img src="data:image/gif;base64,{data_url}" alt="vgg19 preprocessing gif">',unsafe_allow_html=True)
+        cont_2.markdown(f'<img src="data:image/gif;base64,{data_url}" alt="vgg19 preprocessing gif">',unsafe_allow_html=True)
+        cont_2.markdown('''''')
         
-        cont.subheader("""Main results :""")
-        with cont.expander("""Main results :"""):
-            
-            st.markdown("**Loss and global accuracy :**")
+        # Results
+        cont_3 = st.container()
+        cont_3.subheader("""Main results""")
+    
+    
+        cont_3.markdown('''
+                        - ###### Loss and global accuracy :
+        ''')
+        cont_3.markdown(''' Global accuracy on training data : 95%. Global **accuracy on test** and validation data : **94%** ''')
+        with cont_3.expander('Charts'):
             st.image('./data/images/vgg19/vgg19_training.png')
-            
-            st.markdown("Global accuracy on test data : **0.94**")
-            
-            st.markdown("**Classification report :**")
+        
+            st.markdown(''' There is no overfitting. 
+                        The validation loss is much lower than the training loss 
+                        because of dropout layers, data augmentation and class 
+                        weights used during the training.''')
+
+
+        cont_3.markdown('''
+                        - ###### Classification report :
+        ''')
+        cont_3.markdown(''' 
+        F1-Scores greater than 95% for all classes **except for : PMY, MY, MMY and BNE**.
+        ''')
+        with cont_3.expander('Charts'):
             st.image('./data/images/vgg19/vgg19_report.png')
-            
-            st.markdown("**Confusion matrix :**")    
-            st.image('./data/images/vgg19/vgg19_confuse.png', width = 500)
-            
-                      
-        pass
+        
+        
+        
+        
+        cont_3.markdown('''
+                        - ###### Confusion matrix :
+        ''')
+        cont_3.markdown(''' 
+        The main mistake commited by the model is **muddling up the different kind of neutrophiles** (matures and immatures).
+        ''')
+        with cont_3.expander('Charts'):
+            st.image('./data/images/vgg19/vgg19_confuse.png')
+               
+                    
         
     if model_choice == "ViT-b16":
         pass
